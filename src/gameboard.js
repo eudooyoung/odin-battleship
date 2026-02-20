5c5481420e47e5323f9e320f9ff89a580731dde7
Kim Doo Young <177651948+eudooyoung@users.noreply.github.com>
2026-02-20 10:15:11 +0900

    feat: add placeShip function and its test

--- src/gameboard.js
+++ src/gameboard.js
@@ -3,5 +3,24 @@ import Ship from "./ship.js";
 export default class Gameboard {
   ocean;
 
-  constructor() {}
+  constructor() {
+    this.ocean = new Array(10).fill().map(() => new Array(10));
+  }
+
+  placeShip = (coord, length, isVertical = false) => {
+    const row = coord[0];
+    const col = coord[1];
+    const ship = new Ship(length);
+    if (isVertical) {
+      for (let i = 0; i < length; i++) {
+        this.ocean[row + i][col] = ship;
+      }
+    }
+
+    if (!isVertical) {
+      for (let i = 0; i < length; i++) {
+        this.ocean[row][col + i] = ship;
+      }
+    }
+  };
 }
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -2,10 +2,28 @@ import Gameboard from "./gameboard.js";
 import Ship from "./ship.js";
 
 describe("gameboard test", () => {
+  let board;
+
+  beforeEach(() => {
+    board = new Gameboard();
+  });
+
   it("initial state", () => {
     expect(Gameboard).toBeDefined();
 
-    const board = new Gameboard();
     expect(board).toBeInstanceOf(Gameboard);
+    expect(board.ocean).toEqual(new Array(10).fill().map(() => new Array(10)));
+  });
+
+  it("placeShip function", () => {
+    board.placeShip([0, 0], 3, true);
+    expect(board.ocean[0][0]).toBeInstanceOf(Ship);
+    expect(board.ocean[1][0]).toBeInstanceOf(Ship);
+    expect(board.ocean[2][0]).toBeInstanceOf(Ship);
+
+    board.placeShip([0, 1], 3);
+    expect(board.ocean[0][1]).toBeInstanceOf(Ship);
+    expect(board.ocean[0][2]).toBeInstanceOf(Ship);
+    expect(board.ocean[0][3]).toBeInstanceOf(Ship);
   });
 });
