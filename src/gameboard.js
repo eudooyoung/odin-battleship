2eede2153502f6748a84210ff729b73ade1acbbc
Kim Doo Young <177651948+eudooyoung@users.noreply.github.com>
2026-02-20 10:42:58 +0900

    feat: add error handling logic in placeShip function
    If a square has already been occupied by another ship, throw Error.
    If a given coordinate is not it {(x, y) | 0 <= x < 10 and 0 <= y < 10},
    throw RangeError.

--- src/gameboard.js
+++ src/gameboard.js
@@ -10,14 +10,38 @@ export default class Gameboard {
   placeShip = (coord, length, isVertical = false) => {
     const row = coord[0];
     const col = coord[1];
+    if (row < 0 || col < 0) {
+      throw new RangeError("The ship cannot be placed in range");
+    }
+    if (row > 9 || col > 9) {
+      throw new RangeError("The ship cannot be placed in range");
+    }
+
     const ship = new Ship(length);
     if (isVertical) {
+      if (row + length > 9) {
+        throw new RangeError("The ship cannot be placed in range");
+      }
+      for (let i = 0; i < length; i++) {
+        if (this.ocean[row + i][col] instanceof Ship) {
+          throw new Error("The square is already occupied");
+        }
+      }
+
       for (let i = 0; i < length; i++) {
         this.ocean[row + i][col] = ship;
       }
     }
 
     if (!isVertical) {
+      if (col + length > 9) {
+        throw new RangeError("The ship cannot be placed in range");
+      }
+      for (let i = 0; i < length; i++) {
+        if (this.ocean[row][col + i] instanceof Ship) {
+          throw new Error("The square is already occupied");
+        }
+      }
       for (let i = 0; i < length; i++) {
         this.ocean[row][col + i] = ship;
       }
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -17,13 +17,16 @@ describe("gameboard test", () => {
 
   it("placeShip function", () => {
     board.placeShip([0, 0], 3, true);
-    expect(board.ocean[0][0]).toBeInstanceOf(Ship);
-    expect(board.ocean[1][0]).toBeInstanceOf(Ship);
-    expect(board.ocean[2][0]).toBeInstanceOf(Ship);
+    expect(board.ocean[0][0]).toBe(board.ocean[1][0]);
+    expect(board.ocean[1][0]).toBe(board.ocean[2][0]);
 
     board.placeShip([0, 1], 3);
-    expect(board.ocean[0][1]).toBeInstanceOf(Ship);
-    expect(board.ocean[0][2]).toBeInstanceOf(Ship);
-    expect(board.ocean[0][3]).toBeInstanceOf(Ship);
+    expect(board.ocean[0][1]).toBe(board.ocean[0][2]);
+    expect(board.ocean[0][2]).toBe(board.ocean[0][3]);
+
+    expect(() => board.placeShip([0, 0], 3, true)).toThrow(Error);
+    expect(() => board.placeShip([0, 7], 5)).toThrow(RangeError);
+    expect(() => board.placeShip([-1, 3], 3)).toThrow(RangeError);
+    expect(() => board.placeShip([11, 5], 3)).toThrow(RangeError);
   });
 });
