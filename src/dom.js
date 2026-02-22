11bcb4a7839e8fee0981d3d0bc706c126fd4e794
Kim Doo Young <177651948+eudooyoung@users.noreply.github.com>
2026-02-22 14:20:44 +0900

    feat: create basic ui

---
+++ src/dom.js
@@ -0,0 +1,84 @@
+export const header = document.createElement("header");
+export const main = document.createElement("main");
+export const footer = document.createElement("footer");
+
+export const renderHeader = () => {
+  const banner = document.createElement("h1");
+  banner.textContent = "Odin Battleship";
+
+  header.append(banner);
+};
+
+export const renderMain = () => {
+  const boardContainer = document.createElement("div");
+  boardContainer.classList.add("board-container");
+
+  const ocean = renderOcean();
+  const target = renderTarget();
+
+  const buttonContainer = document.createElement("div");
+  buttonContainer.classList.add("button-container");
+  const startButton = document.createElement("button");
+  startButton.textContent = "Start Game";
+  startButton.classList.add("button", "start");
+
+  buttonContainer.append(startButton);
+  boardContainer.append(ocean, target, buttonContainer);
+  main.append(boardContainer);
+};
+
+const renderOcean = () => {
+  const ocean = document.createElement("div");
+  ocean.classList.add("board", "ocean");
+
+  for (let i = 0; i < 11; i++) {
+    for (let j = 0; j < 11; j++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      square.dataset.rows = i;
+      square.dataset.columns = j;
+      ocean.append(square);
+    }
+  }
+
+  renderIndices(ocean);
+
+  return ocean;
+};
+
+const renderTarget = () => {
+  const target = document.createElement("div");
+  target.classList.add("board", "target");
+
+  for (let i = 0; i < 11; i++) {
+    for (let j = 0; j < 11; j++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      square.dataset.rows = i;
+      square.dataset.columns = j;
+      target.append(square);
+    }
+  }
+
+  renderIndices(target);
+
+  return target;
+};
+
+const renderIndices = (board) => {
+  const colIndices = board.querySelectorAll(`[data-rows="0"]`);
+  for (let i = 1; i < colIndices.length; i++) {
+    const colIndice = colIndices[i];
+    colIndice.textContent = String.fromCharCode(64 + i);
+  }
+
+  const rowIndices = board.querySelectorAll(`[data-columns="0"]`);
+  for (let i = 1; i < rowIndices.length; i++) {
+    const rowIndice = rowIndices[i];
+    rowIndice.textContent = i;
+  }
+};
+
+export const renderFooter = () => {
+  footer.textContent = "This is footer";
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("Hello npm!");
+import "./style.css";
+import { header, main, footer } from "./dom.js";
+import { renderHeader, renderMain, renderFooter } from "./dom.js";
+
+const init = () => {
+  const body = document.body;
+
+  renderHeader();
+  renderMain();
+  renderFooter();
+
+  body.append(header, main, footer);
+};
+
+main.addEventListener("click", (e) => {
+  const startButton = e.target.closest("button", "start");
+  if (startButton) {
+  }
+});
+
+init();
